6196ab02f85c7219e18673b04778e4327e7aa0cf
wan <wnh3yang@gmail.com>
2019-06-12 19:14:49 +0800

    a

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
-import MuseUI from 'muse-ui';
+import MuseUI from 'muse-ui'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
-import 'muse-ui/dist/muse-ui.css';
-import 'muse-ui-message/dist/muse-ui-message.css';
+import 'material-design-icons'
+import 'typeface-roboto'
+import 'muse-ui/dist/muse-ui.css'
+import 'muse-ui-message/dist/muse-ui-message.css'
 
 Vue.use(MuseUI);
 
 Vue.config.productionTip = false
 
-// H5的设计稿一般设计为640x1136px即可, 既满足了显示需求，又能降低用户加载图片需要的带宽
 new Vue({
   router,
   render: h => h(App)
